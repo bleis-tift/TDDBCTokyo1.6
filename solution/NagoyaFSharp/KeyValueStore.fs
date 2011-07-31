2416a2248484de306981cd15449bcd719fae241d
bleis-tift <bleis-tift@hotmail.co.jp>
2011-07-31 14:44:22 +0900

    [from now] 2011/07/31 14:44:22 refs #1
    diff --git a/solution/NagoyaFSharp/KeyValueStore.fs b/solution/NagoyaFSharp/KeyValueStore.fs
    index c62794f..0854487 100644
    --- a/solution/NagoyaFSharp/KeyValueStore.fs
    +++ b/solution/NagoyaFSharp/KeyValueStore.fs
    @@ -5,8 +5,10 @@ let empty = []
     let put k v kvs =
       (k, v) :: kvs
    
    -let get k kvs =
    -  None
    +let get key kvs =
    +  kvs
    +  |> List.tryFind (fun (k, _) -> k = key)
    +  |> Option.map snd
    
     let toStr kvs =
       sprintf "%A" kvs

--- solution/NagoyaFSharp/KeyValueStore.fs
+++ solution/NagoyaFSharp/KeyValueStore.fs
@@ -5,8 +5,10 @@ let empty = []
 let put k v kvs =
   (k, v) :: kvs
 
-let get k kvs =
-  None
+let get key kvs =
+  kvs
+  |> List.tryFind (fun (k, _) -> k = key)
+  |> Option.map snd    
 
 let toStr kvs =
   sprintf "%A" kvs
