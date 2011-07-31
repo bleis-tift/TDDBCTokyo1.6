b818d2c23bdd6904ec1649a56ea5a57053c3525a
bleis-tift <bleis-tift@hotmail.co.jp>
2011-07-31 13:48:28 +0900

    空のKVSを生成できるようにした refs #1

---
+++ solution/NagoyaFSharp.Scenario/KeyValueStoreScenario.fs
@@ -0,0 +1,11 @@
+﻿module KeyValueStoreScenario
+
+open NaturalSpec
+open KeyValueStore
+
+[<Scenario>]
+let 空のKVSが生成できる() =
+  Given ()
+  |> When (fun () -> KeyValueStore.empty)
+  |> It should equal []
+  |> Verify
