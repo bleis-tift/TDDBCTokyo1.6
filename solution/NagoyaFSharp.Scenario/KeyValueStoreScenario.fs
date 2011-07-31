4ffcbdbef0b921da4b43cff4324563bef3538c69
bleis-tift <bleis-tift@hotmail.co.jp>
2011-07-31 13:56:15 +0900

    [from now] 2011/07/31 13:56:14 refs #1
    diff --git a/solution/NagoyaFSharp.Scenario/KeyValueStoreScenario.fs b/solution/NagoyaFSharp.Scenario/KeyValueStoreScenario.fs
    index ac9a05d..5623d8e 100644
    --- a/solution/NagoyaFSharp.Scenario/KeyValueStoreScenario.fs
    +++ b/solution/NagoyaFSharp.Scenario/KeyValueStoreScenario.fs
    @@ -10,16 +10,10 @@ let 空のKVSが生成できる() =
       |> It should equal []
       |> Verify
    
    -[<Scenario>]
    -let ``1をキーに、"b"を値に空のKVSに追加すると、[(1, "b")]が返る``() =
    -  Given KeyValueStore.empty
    -  |> When put 1 "b"
    -  |> It should equal [(1, "b")]
    -  |> Verify
    -
    -[<Scenario>]
    -let ``2をキーに、"a"を値を空のKVSに追加すると、[(2, "a")]が返る``() =
    +[<Example(1, "b")>]
    +[<Example(2, "a")>]
    +let ``空のKVSにキーと値をputすると、それのみを含むKVSが返る`` k v =
       Given KeyValueStore.empty
    -  |> When put 2 "a"
    -  |> It should equal [(2, "a")]
    +  |> When put k v
    +  |> It should equal [k, v]
       |> Verify
    \ No newline at end of file

--- solution/NagoyaFSharp.Scenario/KeyValueStoreScenario.fs
+++ solution/NagoyaFSharp.Scenario/KeyValueStoreScenario.fs
@@ -10,16 +10,10 @@ let 空のKVSが生成できる() =
   |> It should equal []
   |> Verify
 
-[<Scenario>]
-let ``1をキーに、"b"を値に空のKVSに追加すると、[(1, "b")]が返る``() =
+[<Example(1, "b")>]
+[<Example(2, "a")>]
+let ``空のKVSにキーと値をputすると、それのみを含むKVSが返る`` k v =
   Given KeyValueStore.empty
-  |> When put 1 "b"
-  |> It should equal [(1, "b")]
-  |> Verify
-
-[<Scenario>]
-let ``2をキーに、"a"を値を空のKVSに追加すると、[(2, "a")]が返る``() =
-  Given KeyValueStore.empty
-  |> When put 2 "a"
-  |> It should equal [(2, "a")]
+  |> When put k v
+  |> It should equal [k, v]
   |> Verify
