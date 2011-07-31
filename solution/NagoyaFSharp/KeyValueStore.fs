eb29ebf2e561630db55c901bcf7f63eba19a0beb
bleis-tift <bleis-tift@hotmail.co.jp>
2011-07-31 16:53:54 +0900

    APIを変更 refs #5

--- solution/NagoyaFSharp/KeyValueStore.fs
+++ solution/NagoyaFSharp/KeyValueStore.fs
@@ -8,12 +8,12 @@ let now () = match dt with Some dt -> dt | None -> DateTime.Now
 
 let empty = []
 
-let init kvs = kvs
+let init kvs = kvs |> List.map (fun (k, v) -> k, v, now())
 
-let private eq key (k, _) = k = key
+let private eq key (k, _, _) = k = key
 
 let put k v kvs =
-  (k, v) :: (kvs |> List.filter (eq k >> not))
+  (k, v, now()) :: (kvs |> List.filter (eq k >> not))
 
 let putAll xs kvs =
   xs |> List.fold (fun st (k, v) -> st |> put k v) kvs
@@ -21,7 +21,7 @@ let putAll xs kvs =
 let get key kvs =
   kvs
   |> List.tryFind (eq key)
-  |> Option.map snd    
+  |> Option.map (fun (_, v, _) -> v)
 
 let delete key kvs =
   kvs |> List.filter(eq key >> not) 
