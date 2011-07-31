3a2a5db67f0662fc66d7087f6c30c6d2a1759269
bleis-tift <bleis-tift@hotmail.co.jp>
2011-07-31 16:54:09 +0900

    APIの変更に合わせてテストを修正して、再構築終了 refs #5

--- solution/NagoyaFSharp.Scenario/KeyValueStoreScenario.fs
+++ solution/NagoyaFSharp.Scenario/KeyValueStoreScenario.fs
@@ -1,7 +1,14 @@
 ﻿module KeyValueStoreScenario
 
 open NaturalSpec
+open NUnit.Framework
 open KeyValueStore
+
+let defaultDt = System.DateTime(2011, 7, 31)
+
+[<SetUp>]
+let setup () =
+  KeyValueStore.dt <- Some defaultDt
 
 [<Scenario>]
 let 空のKVSが生成できる() =
@@ -13,7 +20,7 @@ let 空のKVSが生成できる() =
 [<Scenario>]
 let ペアを複数登録したKVSが生成できる() =
   Given KeyValueStore.init [(1, "a"); (2, "b")]
-  |> It should equal [(1, "a"); (2, "b")]
+  |> It should equal [(1, "a", defaultDt); (2, "b", defaultDt)]
   |> Verify
 
 [<Example(1, "b")>]
@@ -21,7 +28,7 @@ let ペアを複数登録したKVSが生成できる() =
 let ``空のKVSにペアをputすると、それのみを含むKVSが返る`` k v =
   Given KeyValueStore.empty
   |> When put k v
-  |> It should equal [k, v]
+  |> It should equal [k, v, defaultDt]
   |> Verify
 
 [<Example(2, "b")>]
@@ -29,14 +36,14 @@ let ``空のKVSにペアをputすると、それのみを含むKVSが返る`` k 
 let ``(1, "a")のみを含むKVSに重複しないペアをputすると、putしたペアが追加されたKVSが返る`` k v =
   Given KeyValueStore.init [1, "a"]
   |> When put k v
-  |> It should equal [k, v; 1, "a"]
+  |> It should equal [k, v, defaultDt; 1, "a", defaultDt]
   |> Verify
 
 [<Scenario>]
 let ``既に存在するキーを含むペアをputすると、値が更新される``() =
   Given KeyValueStore.init [1, "a"; 2, "b"; 3, "c"]
   |> When put 2 "hoge"
-  |> It should equal [2, "hoge"; 1, "a"; 3, "c"]
+  |> It should equal [2, "hoge", defaultDt; 1, "a", defaultDt; 3, "c", defaultDt]
   |> Verify
 
 [<Scenario>]
@@ -50,7 +57,7 @@ let 空のKVSをtoStrで文字列化できる() =
 let ペアを一つ含むKVSをtoStrで文字列化できる() =
   Given KeyValueStore.init ["a", 10]
   |> When toStr
-  |> It should equal @"[(""a"", 10)]"
+  |> It should equal @"[(""a"", 10, 2011/07/31 0:00:00)]"
   |> Verify
 
 [<Scenario>]
@@ -85,26 +92,26 @@ let 空のKVSでdeleteしても空のまま() =
 let 存在するキーを指定してdeleteすると取り除く() =
   Given KeyValueStore.init [1, "a"; 2, "b"; 10, ""]
   |> When delete 2
-  |> It should equal [1, "a"; 10, ""]
+  |> It should equal [1, "a", defaultDt; 10, "", defaultDt]
   |> Verify
 
 [<Scenario>]
 let putAllで複数登録できる() =
   Given KeyValueStore.init ["hoge", "piyo"; "foo", "bar"]
   |> When putAll ["a", "aaa"; "b", "bbb"]
-  |> It should equal ["b", "bbb"; "a", "aaa"; "hoge", "piyo"; "foo", "bar"]
+  |> It should equal ["b", "bbb", defaultDt; "a", "aaa", defaultDt; "hoge", "piyo", defaultDt; "foo", "bar", defaultDt]
   |> Verify
 
 [<Scenario>]
 let 既に存在するキーは値が上書きされる() =
   Given KeyValueStore.init [1, "hoge"; 2, "piyo"; 3, "foo"; 4, "bar"]
   |> When putAll [1, "aaa"; 3, "bbb"]
-  |> It should equal [3, "bbb"; 1, "aaa"; 2, "piyo"; 4, "bar"]
+  |> It should equal [3, "bbb", defaultDt; 1, "aaa", defaultDt; 2, "piyo", defaultDt; 4, "bar", defaultDt]
   |> Verify
 
 [<Scenario>]
 let 指定した引数内にキーの重複がある場合後勝ち() =
   Given KeyValueStore.empty
   |> When putAll [1, "aaa"; 2, "bbb"; 3, "ccc"; 2, "hoge"]
-  |> It should equal [2, "hoge"; 3, "ccc"; 1, "aaa"]
+  |> It should equal [2, "hoge", defaultDt; 3, "ccc", defaultDt; 1, "aaa", defaultDt]
   |> Verify
