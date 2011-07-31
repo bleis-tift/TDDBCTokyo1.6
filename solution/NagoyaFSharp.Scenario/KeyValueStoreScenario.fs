c01bbe565c45faaeebc517c8b55852416268181d
bleis-tift <bleis-tift@hotmail.co.jp>
2011-07-31 13:53:22 +0900

    [from now] 2011/07/31 13:53:22 refs #1
    diff --git a/solution/NagoyaFSharp.Scenario/KeyValueStoreScenario.fs b/solution/NagoyaFSharp.Scenario/KeyValueStoreScenario.fs
    index ae87e78..ac9a05d 100644
    --- a/solution/NagoyaFSharp.Scenario/KeyValueStoreScenario.fs
    +++ b/solution/NagoyaFSharp.Scenario/KeyValueStoreScenario.fs
    @@ -15,4 +15,11 @@ let ``1をキーに、"b"を値に空のKVSに追加すると、[(1, "b")]が返
       Given KeyValueStore.empty
       |> When put 1 "b"
       |> It should equal [(1, "b")]
    +  |> Verify
    +
    +[<Scenario>]
    +let ``2をキーに、"a"を値を空のKVSに追加すると、[(2, "a")]が返る``() =
    +  Given KeyValueStore.empty
    +  |> When put 2 "a"
    +  |> It should equal [(2, "a")]
       |> Verify
    \ No newline at end of file

--- solution/NagoyaFSharp.Scenario/KeyValueStoreScenario.fs
+++ solution/NagoyaFSharp.Scenario/KeyValueStoreScenario.fs
@@ -16,3 +16,10 @@ let ``1をキーに、"b"を値に空のKVSに追加すると、[(1, "b")]が返
   |> When put 1 "b"
   |> It should equal [(1, "b")]
   |> Verify
+
+[<Scenario>]
+let ``2をキーに、"a"を値を空のKVSに追加すると、[(2, "a")]が返る``() =
+  Given KeyValueStore.empty
+  |> When put 2 "a"
+  |> It should equal [(2, "a")]
+  |> Verify
